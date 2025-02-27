d0edaf02418b3ed49e0ff428640268e0063218ab
geoffrich94 <geoffrey.richardson94@gmail.com>
2025-02-27 21:40:43 +0000

    new url

--- src/components/PlayerSummary/PlayerSummary.styles.ts
+++ src/components/PlayerSummary/PlayerSummary.styles.ts
@@ -26,7 +26,7 @@ export const Container = styled.div`
 
 export const ProfilePicture = styled.div<{ profilePicture: string }>`
   width: 22%;
-  background: url(${({ profilePicture }) => `assets/${profilePicture}-profile-pic.png`});
+  background: url(${({ profilePicture }) => `${process.env.PUBLIC_URL || ""}${profilePicture}-profile-pic.png`});
   background-repeat: no-repeat;
   background-attachment: fixed;
   background-size: cover;
